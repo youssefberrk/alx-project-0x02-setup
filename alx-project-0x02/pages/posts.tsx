16ed50c91f808357444dcb08c46bbf14e274d78f
youssef <yberrakouan.lol@gmail.com>
2025-01-08 13:02:16 +0100

    Implement a PostCard Component fixed

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -3,24 +3,7 @@ import PostCard from "../components/common/PostCard";
 import { PostProps } from "../interfaces";
 import Header from "@/components/layout/Header";
 
-const PostsPage: React.FC = () => {
-  const [posts, setPosts] = useState<PostProps[]>([]);
-
-  // Fetch data from JSONPlaceholder API
-  useEffect(() => {
-    const fetchPosts = async () => {
-      const response = await fetch(
-        "https://jsonplaceholder.typicode.com/posts"
-      );
-      const data = await response.json();
-      setPosts(data);
-    };
-    try {
-      fetchPosts();
-    } catch (error) {
-      console.error("Error fetching posts:", error);
-    }
-  }, []);
+const PostsPage: React.FC<{ posts: PostProps[] }> = ({ posts }) => {
   return (
     <div>
       <Header />
@@ -39,6 +22,18 @@ const PostsPage: React.FC = () => {
       </div>
     </div>
   );
+};
+
+export default PostsPage;
+export const getStaticProps = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts = await response.json();
+
+  return {
+    props: {
+      posts,
+    },
+  };
 };
 
 export default PostsPage;
